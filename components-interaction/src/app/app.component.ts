8bea3acede5d2c29cf545c8fd52392e7b1e0d7ab
Rafal <rmf@europe.com>
2020-02-27 06:27:33 +0100

    basic template

--- components-interaction/src/app/app.component.ts
+++ components-interaction/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'interaction';
+  title1: string = 'Angular-In-Examples';
+  title2: string = '...................';
 }
